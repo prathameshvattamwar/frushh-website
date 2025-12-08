612ba1c8400e9395deb2b0d43b1e9c059d20b359
Prathamesh Vattamwar <117809192+prathameshvattamwar@users.noreply.github.com>
2025-12-08 23:42:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { supabase } from './lib/supabase'
+import Quiz from './components/Quiz'
 
 function App() {
   const [products, setProducts] = useState([])
